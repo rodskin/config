23bb97741fffd48b3c26ebfe5835f98c863b85a6
fgilles <fgilles@ds-agency.com>
2020-03-05 14:46:36 +0100

    added config infos

--- install.sh
+++ install.sh
@@ -1,4 +1,29 @@
-#!/bin/sho
+#!/bin/sh
+
+########
+# COLORS
+########
+NORMAL="\033[0m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+BROWN="\033[0;33m"
+BLUE="\033[0;34m"
+CYAN="\033[0;36m"
+YELLOW="\033[0;33m"
+LIGHT_RED="\033[1;31m"
+LIGHT_GREEN="\033[1;32m"
+LIGHT_BLUE="\033[1;34m"
+LIGHT_YELLOW="\033[1;33m"
+WHITE="\033[1;37m"
+BLACK="\033[0;30m"
+MAGENTA="\033[1;35m"
+LIGHT_CYAN="\033[1;36m"
+LIGHT_GRAY="\033[0;37m"
+GRAY="\033[01;30m"
+BOLD="\033[1m"
+UNDERSCORE="\033[4m"
+REVERSE="\033[7m"
+
 cd ~/
 
 sudo apt install git
